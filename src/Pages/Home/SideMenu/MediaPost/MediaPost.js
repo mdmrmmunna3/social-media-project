54d82d79251ddc3a19b2a8c255d7705efb383089
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-12-29 00:09:30 +0600

    imgbb uploaded

--- src/Pages/Home/SideMenu/MediaPost/MediaPost.js
+++ src/Pages/Home/SideMenu/MediaPost/MediaPost.js
@@ -2,29 +2,36 @@ import React, { useContext } from 'react';
 import { FcPicture } from "react-icons/fc";
 import { FaRegLaugh, FaRegUserCircle } from "react-icons/fa";
 import { AuthContext } from '../../../../contexts/AuthProvider/AuthProvider';
+import { toast } from 'react-hot-toast';
+import { useForm } from 'react-hook-form';
 
 const MediaPost = () => {
 
     const { user } = useContext(AuthContext);
+    const { handleSubmit, register, formState: { errors } } = useForm();
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const form = e.target;
-        const post = form.post.value;
-        const media = form.photo_video.value;
-        console.log(post, media)
+    const imgHostKey = process.env.REACT_APP_imgbb_key;
 
-        // const image = e.image;
-        // const formData = new FormData();
-        // formData.append('image', image)
+    const handleMediaSubmit = data => {
+        // e.preventDefault();
+        // const form = e.target;
+        // const post = form.post.value;
+        // // const image = form.photo_video.value;
+        // console.log(post)
 
-        // fetch(url, {
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image)
+        const url = `https://api.imgbb.com/1/upload?key=${imgHostKey}`
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(ImageData => {
+                console.log(ImageData)
+            })
 
-        // })
-        // .then(res => res.json())
-        // .then(data => console.log(ImageData.data.url))
-
-        form.reset();
     }
 
     return (
@@ -51,7 +58,7 @@ const MediaPost = () => {
                     {/* Put this part before </body> tag */}
                     <input type="checkbox" id="my-modal-3" className="modal-toggle" />
                     <div className="modal">
-                        <form onSubmit={handleSubmit} className="modal-box relative" style={{ backgroundColor: "rgb(36 37 34)" }}>
+                        <form onSubmit={handleSubmit(handleMediaSubmit)} className="modal-box relative" style={{ backgroundColor: "rgb(36 37 34)" }}>
                             <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                             <h3 className="text-2xl text-white font-bold text-center">Create Post</h3>
                             <div className='text-white flex'>
@@ -62,13 +69,36 @@ const MediaPost = () => {
                                     user?.uid ? <h3>{user?.displayName}</h3> : <p className='ml-3'>unknown</p>
                                 }
                             </div>
-                            <textarea name="post" className="textarea  w-full mt-3 text-white text-xl" style={{ backgroundColor: "rgb(36 37 38)" }} placeholder="What's on your mind"></textarea>
 
-                            <div className='mt-6'>
+
+
+                            {/* <textarea name="post" className="textarea  w-full mt-3 text-white text-xl" style={{ backgroundColor: "rgb(36 37 38)" }} placeholder={`What's on your mind ${user?.displayName}`}></textarea> */}
+
+                            {/* <div className='mt-6'>
                                 <span className='text-white font-semibold mr-4'>Add to your Post</span>
-                                <input type="file" name='photo_video' className="file-input file-input-bordered file-input-info w-full max-w-xs" />
+                                <input type="file" name='image' className="file-input file-input-bordered file-input-info w-full max-w-xs" />
+                            </div> */}
+
+                            <textarea
+                                {...register("textPost", {
+                                    required: 'upload your text'
+                                })
+                                }
+                                className="textarea  w-full mt-3 text-white text-xl" required style={{ backgroundColor: "rgb(36 37 38)" }} />
+                            {errors.img && <p className='text-red-600'>{errors.img?.message}</p>}
+
+                            <div className="form-control block mt-6">
+                                <span className='text-white font-semibold mr-4'>Add to your Post</span>
+                                <input type="file"
+                                    {...register("image", {
+                                        required: 'upload your media'
+                                    })
+                                    }
+                                    className="file-input file-input-bordered file-input-info w-full max-w-xs" />
+                                {errors.img && <p className='text-red-600'>{errors.img?.message}</p>}
                             </div>
-                            <button className='btn w-full mt-2 btn-primary text-red font-bold' >Submit</button>
+
+                            <button className='btn w-full mt-3 btn-primary text-red font-bold' >Submit</button>
                         </form>
                     </div>
                 </div>
